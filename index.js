825a8dada7a0784319eb02882c5f7f55ce0a7e69
Mujahid corporate prism <c.prism2002@gmail.com>
2025-07-03 15:59:11 +0500

    port changes

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ app.use("/api/bills", require("./routes/billRoutes"));
 const PORT = 8080;
 
 // listen
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`.bgCyan.white);
 });
